a0ddbffdf1ce23023201b544903b09139562da34
MN Chaniago <mnurchaniago97@gmail.com>
2026-02-20 21:40:31 +0700

    Update auth branding and favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ $title ?? 'Dashboard' }} | KSR PMI UNP</title>
+    <link rel="icon" type="image/png" href="/images/logo/logoksr.png">
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
